956cd1cf4c011ebeea4af1be272262711129fe5c
berekettena <tena21bereket@gmail.com>
2024-04-09 15:59:07 +0300

    requirement fixed

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -16,11 +16,12 @@ class Config(object):
     config class
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCAL = 'en'
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
+"""""" Use that class as config for Flask app """
 
 
 @app.route('/')
